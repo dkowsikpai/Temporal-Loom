9022f6c5e9243a46c4dbc69f8e8690197fcd25b9
dkowsikpai <dkowsikpai@gmail.com>
2023-10-28 00:10:23 +0530

    Restructuing Dataset

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ bash install.sh
 
 ## Dataset
 Create a folder named `data` after cloning this repository and download the [TempLAMA](https://github.com/google-research/language/tree/master/language/templama) dataset.
+There are a total of 50,310 samples after the combing of the data. 
+
+### Analysis
+Number of datasamples that have changed over time (unique queries) = 5,823
+
+More are available in the [Google Colab](https://colab.research.google.com/drive/1Rwz7tQKBNxo8l-gSoP8dX21rWBXLNeRn?usp=sharing)
 
 
 ## Execution of Code
@@ -23,3 +29,4 @@ Use the following script
 python src/combine_restructure_data.py --input ./data/train.json,./data/test.json,./data/val.json
 ```
 > Note: You can add more data by seperating the data path in `--input` parameter by comma.
+
--- src/combine_restructure_data.py
+++ src/combine_restructure_data.py
@@ -1,6 +1,8 @@
 # Importing necessary libraries
 import jsonlines
+from tqdm import tqdm
 import argparse
+from rich.pretty import pprint
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--input', help='Input file path')
@@ -24,3 +26,57 @@ for i in range(len(files)):
 print("Writing the output file")
 with jsonlines.open('./data/combined_data.json', mode='w') as writer:
     writer.write_all(output)
+
+
+# Sample data
+"""{
+   "query":"Valentino Rossi plays for _X_.",
+   "answer":[
+      {
+         "wikidata_id":"Q1085474",
+         "name":"Yamaha Motor Racing"
+      }
+   ],
+   "date":"2010",
+   "id":"Q169814_P54_2010",
+   "most_frequent_answer":{
+      "wikidata_id":"Q1085474",
+      "name":"Yamaha Motor Racing"
+   },
+   "most_recent_answer":{
+      "wikidata_id":"Q1085474",
+      "name":"Yamaha Motor Racing"
+   },
+   "relation":"P54"
+}"""
+
+
+# Restructuring the data
+print("Restructuring the data")
+grouped_data = {} # Keys will be the query and values will be dict of answers over time
+for i in tqdm(output):
+  if i["query"] not in grouped_data:
+    grouped_data[i["query"]] = []
+  grouped_data[i["query"]].append({"answer": i["answer"][0]["name"], "date": i["date"]})
+
+# Removing repetitive duplicates
+print("Number of samples before filtering: {}".format(len(grouped_data)))
+filtered = {}
+for k, v in grouped_data.items():
+  if len(v) < 12:
+    filtered[k] = v
+grouped_data = filtered.copy()
+print("Number of samples after filtering: {}".format(len(grouped_data)))
+
+
+# JSONing
+data = []
+for k, v in grouped_data.items():
+  data.append({"query": k, "answer": v})
+
+
+# Writing the output file
+print("Writing the output file")
+with jsonlines.open('./data/restructured_data.json', mode='w') as writer:
+    writer.write_all(data)
+
