a7643222b0b9237d05cc24cba99bd4cdc662a2b0
dkowsikpai <dkowsikpai@gmail.com>
2023-10-28 00:17:53 +0530

    Update

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ conda create -n temporal python=3.8
 conda activate temporal
 pip install -r requirements.txt
 
+mkdir data
+cd data
+wget https://storage.googleapis.com/gresearch/templama/train.json
+wget https://storage.googleapis.com/gresearch/templama/val.json
+wget https://storage.googleapis.com/gresearch/templama/test.json
+cd ../
